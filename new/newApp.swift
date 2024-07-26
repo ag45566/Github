8d33aa21dc7724e6f00b15b32a4aa72f029a9aa7
Alicia Garcia <alicimusic19@icloud.com>
2024-07-26 13:18:38 -0400

    Initial Commit

---
+++ new/newApp.swift
@@ -0,0 +1,17 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by Alicia Garcia on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
